3e4a8421d856ac546538b8c1e47eeeac4b8402ba
mayurpawar0809 <maxmayur686@gmail.com>
2021-07-30 09:37:32 +0530

    bootstrap

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -2,35 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
- export default function App() {
-  //let list =["hello","hello"];
-
-
-  //declaring variable usin react::hooks
-  //array destructuring
-
-  const [list,setList]= useState([""]);
-
-
-//arrow function
-  const addNewHello = () => {
-   // list.push("hello");
-   const newList=[...list,"hello"];
-   setList(newList);
-
-    console.log(list);
-  };
-
-
-
-  //view
+export default function App() {
   return (
     <div>
-      <h1>workin with event </h1>
-      <input type="button" value="say hello" onClick={addNewHello} />
-      {list.map((item) => {
-        return <h3>{item} </h3>;
-      })}
+     
+      <h1 className="bg-primary text-light p-2">my</h1>
     </div>
   );
 }
