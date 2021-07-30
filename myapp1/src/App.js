4c163931754759609c56223a1421ef1ea7507f51
mayurpawar0809 <maxmayur686@gmail.com>
2021-07-30 08:55:48 +0530

    final adding done

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
- function App() {
-  let list =["hello","hello"];
+ export default function App() {
+  //let list =["hello","hello"];
 
+
+  //declaring variable usin react::hooks
+  //array destructuring
+
+  const [list,setList]= useState([""]);
+
+
+//arrow function
   const addNewHello = () => {
-    list.push("hello");
+   // list.push("hello");
+   const newList=[...list,"hello"];
+   setList(newList);
+
     console.log(list);
   };
 
@@ -23,4 +35,4 @@ import "./App.css";
   );
 }
 
-export default App;
+//export default App;
